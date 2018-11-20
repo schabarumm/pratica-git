e4583a136eb35e5afb6928cc745280f9fe9807e0
jeferson <xeevy.sjo@gmail.com>
2018-11-19 21:17:39 -0300

    MODIFICATION EQUIVOCAL

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php 
-  phpinfo()
+aqui sexta feira 13 é hoje é amanha, todo dia o jason anda de quadrado e titan, o brinquedo assassino ta com a 9 cromada, de rd fazendo halloween na quebrada, psicose na mente aterroriza na rima
+  
+phpinfo()
 ?>
